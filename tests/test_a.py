e79c779b0355e3a94a0c9918bea9183466ef0a19
davewd <davewd@me.com>
2019-04-03 22:00:25 +1100

    correct assignment to ==

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,7 +1,7 @@
 import python_cheatsheet.a as a
 
 def test_PublicFn(self) :
-    assert a.public_fn_with_googley_docstring("test") = 0
+    assert a.public_fn_with_googley_docstring("test") == 0
 
 if __name__ == "__main__" :
     unittest.main(verbosity=2, exit=False);

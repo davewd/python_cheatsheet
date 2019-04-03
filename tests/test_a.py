386557ad2b5821cb8f0dd059728e6fa6590b4517
davewd <davewd@me.com>
2019-04-03 21:37:06 +1100

    First Unit Test !

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,1 +1,9 @@
 import unittest
+import python_cheatsheet.a as a
+
+class TestA ( unittest.TestCase ) :
+    def testPublicFn(self) :
+        self.assertEqual(a.public_fn_with_googley_docstring("test"), 0 )
+
+if __name__ == "__main__" :
+    unittest.main(verbosity=2, exit=False);

52c15a9fc745afdc1ee0743021ea89e5169b48e4
davewd <davewd@me.com>
2019-04-03 21:57:17 +1100

    switch back to pytest

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,9 +1,7 @@
-import unittest
 import python_cheatsheet.a as a
 
-class TestA ( unittest.TestCase ) :
-    def testPublicFn(self) :
-        self.assertEqual(a.public_fn_with_googley_docstring("test"), 0 )
+def test_PublicFn(self) :
+    assert a.public_fn_with_googley_docstring("test") = 0
 
 if __name__ == "__main__" :
     unittest.main(verbosity=2, exit=False);

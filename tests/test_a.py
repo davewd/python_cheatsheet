a303c3ebb179d11741924f9d570f5095d94cfcbd
davewd <davewd@me.com>
2019-04-04 16:10:13 +1100

    Add Unit test for Exceptions

--- tests/test_a.py
+++ tests/test_a.py
@@ -3,6 +3,3 @@ import python_cheatsheet.a as a
 def test_PublicFn() :
     assert a.public_fn_with_googley_docstring("test") == 0
 
-if __name__ == "__main__" :
-    unittest.main(verbosity=2, exit=False);
-
